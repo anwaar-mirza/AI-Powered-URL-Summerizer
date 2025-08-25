0338d28f514820588cabf99704f114eda922cf6c
Anwaar Mirza <mirzaanwaar52@gmail.com>
2025-08-25 12:00:51 +0500

    Update backend.py

--- backend.py
+++ backend.py
@@ -2,16 +2,12 @@ import streamlit as st
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from essentials import *
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
 
-os.environ['GROQ_API_KEY'] = st.secrets['GROQ_API_KEY']
 
 class YouTubeSummarizer:
-    def __init__(self, prompt):
-        llm = ChatGroq(model="llama-3.3-70b-versatile")
+    def __init__(self, prompt, key):
+        llm = ChatGroq(model="llama-3.3-70b-versatile", api_key=key)
         chat_prompt = ChatPromptTemplate.from_template(prompt)
         self.chain = chat_prompt | llm
        
